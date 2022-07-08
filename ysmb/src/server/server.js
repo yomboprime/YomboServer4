3644c6e414c3aa0556cd22af4b51f36b1986cd1b
yomboprime <juanjoluna@google.com>
2022-07-08 17:59:22 +0200

    -

--- ysmb/src/server/server.js
+++ ysmb/src/server/server.js
@@ -101,7 +101,7 @@ function createWebServer() {
 
 			console.log( "Client connected." );
 
-			const params = webAppServer..getURLParameters( client.req.url );
+			const params = webAppServer.getURLParameters( client.req.url );
 			let token = null;
 			for ( let i = 0; i < params.length; i++ ) {
 				if ( params[ i ].name === 'accessToken' ) {
